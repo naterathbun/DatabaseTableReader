a49a7946aa68ee5c378c67a19c529a9e0d5b7751
Nathan Rathbun <naterathbun@gmail.com>
2017-02-12 20:18:46 -0500

    Added barebones code to connect to a DB

--- DatabaseTableReader/DatabaseTableReader/Program.cs
+++ DatabaseTableReader/DatabaseTableReader/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
 
 namespace DatabaseTableReader
 {
@@ -10,6 +12,25 @@ namespace DatabaseTableReader
     {
         static void Main(string[] args)
         {
+            string dataBaseSourceInformation = GetDataBaseSource();
+
+            using (SqlConnection sqlConnection = new SqlConnection(dataBaseSourceInformation))
+            {
+                sqlConnection.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM country;", sqlConnection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(Convert.ToString(reader["name"]));
+                }
+
+            }
+        }
+
+        private static string GetDataBaseSource()
+        {
+            return @"Data Source=.\SQLEXPRESS;Initial Catalog=world;User ID=te_student;Password=sqlserver1";
         }
     }
 }
